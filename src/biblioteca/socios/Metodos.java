ee74d0981778f3fe0068be6d9e347180b7f02a59
mbacelofernandez <mbacelofernandez@victoria12.danielcastelao.org>
2018-03-13 11:07:07 +0100

    metodo borrar socio creado

--- src/biblioteca/socios/Metodos.java
+++ src/biblioteca/socios/Metodos.java
@@ -4,23 +4,57 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+import libreria.Biblioteca;
 
 public class Metodos {
-    
-    static File ficheiro;
+
+    static File fichero;
     static FileWriter escribir;
-    
-    public static void añadirSocio(Socio nuevoSocio) throws IOException{
+    static Scanner sc;
+    static PrintWriter esc;
+
+    public static void añadirSocio(Socio nuevoSocio) throws IOException {
         try {
-            ficheiro = new File("socios.txt");
-            escribir = new FileWriter(ficheiro, true);
-            escribir.write(nuevoSocio.guardarSocio(nuevoSocio)+"\n");
+            fichero = new File("socios.txt");
+            escribir = new FileWriter(fichero, true);
+            escribir.write(nuevoSocio.guardarSocio(nuevoSocio) + "\n");
         } catch (FileNotFoundException ex) {
-            System.out.println("ERROR! "+ex.getMessage());
-        }
-        finally{
+            System.out.println("ERROR! " + ex.getMessage());
+        } finally {
             escribir.close();
         }
     }
-    
+
+    public static void borrarSocio(String dni) throws IOException {
+        String linea;
+        File fichero2 = new File("aux.txt");
+        fichero = new File("socios.txt");
+        try {
+            esc = new PrintWriter(fichero2);
+            sc = new Scanner(fichero);
+            while (sc.hasNextLine()) {
+                linea = sc.nextLine();
+                String[] lista = linea.split(";");
+                if (!lista[2].equalsIgnoreCase(dni)) {
+                    Socio nuevoSocio = new Socio(lista[0], lista[1], lista[2], lista[3], lista[4]);
+                    esc.write(nuevoSocio.guardarSocio(nuevoSocio) + "\n");
+                }
+            }
+            boolean flag = fichero2.renameTo(fichero);
+            if (flag) {
+                Biblioteca.mostrarMensaje("El renombrado ha sido correcto");
+
+            } else {
+                Biblioteca.mostrarMensaje("El renombrado no se ha podido realizar");
+            }
+        } catch (FileNotFoundException ex) {
+            System.out.println("ERROR! " + ex.getMessage());
+        } finally {
+            escribir.close();
+            sc.close();
+        }
+    }
 }
